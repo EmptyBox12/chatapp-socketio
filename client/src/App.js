0c9431c9a8d16f2a96f6f19e64d5cfa8eff4cd61
EmptyBox12 <ataberktumay@gmail.com>
2022-01-22 23:16:06 +0300

    basic chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import socketClient  from "socket.io-client";
+import React, {useEffect, useState} from "react";
+
+let socket = socketClient ("http://localhost:3001/");
+
 
 function App() {
+  const [messages, setMessages] = useState(["cat"]);
+  const [chatMessage, setChatMessage] = useState("");
+
+  useEffect(()=> {
+    socket.on("message", (message)=> {
+      let arr = [...messages];
+      arr.push(message);
+      if(messages !== arr){
+        setMessages(arr);
+      }
+    })
+  },[messages])
+
+  function handleSend(e) {
+    e.preventDefault();
+    socket.emit("chatMessage", chatMessage);
+    setChatMessage("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {messages.map((message, index) => {
+        return (<div key={index}>{message}</div>);
+      })}
+      <form onClick={handleSend}>
+        <input type="text" value={chatMessage} onChange={(e)=>{setChatMessage(e.target.value)}}/>
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,10 +3,21 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: "*",
+});
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  socket.emit("message", "Welcome to the chat!");
+
+  socket.on("chatMessage", (message)=> {
+    io.emit("message", message);
+  })
+
+  socket.on("disconnect", ()=> {
+    io.emit("message", "User has left the chat");
+  })
+
 });
 
 server.listen(3001, () => {
