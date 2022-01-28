fc5de7294b12c57f50eb19a41ea13cf3751e6bde
EmptyBox12 <ataberktumay@gmail.com>
2022-01-28 23:34:40 +0300

    add css to chatroom, add support for nested messages

--- client/src/components/ChatRoom.js
+++ client/src/components/ChatRoom.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 export default function ChatRoom({
   username,
@@ -9,6 +9,8 @@ export default function ChatRoom({
 }) {
   const [messages, setMessages] = useState([]);
   const [chatMessage, setChatMessage] = useState("");
+  const ownRef = useRef();
+  const messageRef = useRef();
 
   useEffect(() => {
     socket.emit("join-room", { username, roomName });
@@ -18,8 +20,16 @@ export default function ChatRoom({
   useEffect(() => {
     socket.on("message", (message) => {
       setMessages((prevState) => {
+        if (message.id && message.id === prevState[prevState.length - 1].id) {
+          message.username = "";
+        }
         return [...prevState, message];
       });
+      if (username == message.username) {
+        ownRef.current.scrollIntoView();
+      } else {
+        messageRef.current.scrollIntoView();
+      }
     });
   }, []);
 
@@ -30,35 +40,47 @@ export default function ChatRoom({
   }
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          socket.emit("leave-room");
-          setUsername("");
-          setRoomName("");
-        }}
-      >
-        Back
-      </button>
-      {messages.map((message, index) => {
-        return message.id && socket.id === message.id ? (
-          <div key={index}>You: {message.text}</div>
-        ) : (
-          <div key={index}>
-            {message.username}: {message.text}
-          </div>
-        );
-      })}
-      <form onSubmit={handleSend}>
-        <input
-          type="text"
-          value={chatMessage}
-          onChange={(e) => {
-            setChatMessage(e.target.value);
+    <div className="chatContainer">
+      <div className="title chatRoom">
+        <div>ChatApp</div>
+        <button
+          onClick={() => {
+            socket.emit("leave-room");
+            setUsername("");
+            setRoomName("");
           }}
-        />
-        <button type="submit">Send</button>
-      </form>
+        >
+          Back
+        </button>
+      </div>
+      <div className="chatMain">
+        <div className="chatMessages">
+          {messages.map((message, index) => {
+            return message.id && socket.id === message.id ? (
+              <div ref={ownRef} className="ownMessage" key={index}>
+                {message.text}
+              </div>
+            ) : (
+              <div ref={messageRef} className="message" key={index}>
+                <div className="messageUsername">{message.username}</div>
+                <div className="messageContent">{message.text}</div>
+              </div>
+            );
+          })}
+        </div>
+
+        <form onSubmit={handleSend}>
+          <input
+            required="true"
+            type="text"
+            value={chatMessage}
+            onChange={(e) => {
+              setChatMessage(e.target.value);
+            }}
+          />
+          <button type="submit"></button>
+        </form>
+      </div>
     </div>
   );
 }
